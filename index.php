50b00e21940e21ff66dc0b96f212f33fd04d19b8
Max Tkachenko <manutd20x@gmail.com>
2017-06-29 19:06:03 +0300

    random function

--- index.php
+++ index.php
@@ -13,37 +13,38 @@ $token = "390875011:AAEkDpRCBeIiRQ3clQ03PcEDty5QzEwtb60";
 $bot = new \TelegramBot\Api\Client($token);
 
 // если бот еще не зарегистрирован - регистрируем
-if(!file_exists("registered.trigger")){ 
+if (!file_exists("registered.trigger")) {
 	/**
-	 * файл registered.trigger будет создаваться после регистрации бота. 
-	 * если этого файла нет значит бот не зарегистрирован 
+	 * файл registered.trigger будет создаваться после регистрации бота.
+	 * если этого файла нет значит бот не зарегистрирован
 	 */
-	 
+
 	// URl текущей страницы
-	$page_url = "https://".$_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
+	$page_url = "https://" . $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"];
 	$result = $bot->setWebhook($page_url);
-	if($result){
-		file_put_contents("registered.trigger",time()); // создаем файл дабы прекратить повторные регистрации
+	if ($result) {
+		file_put_contents("registered.trigger", time()); // создаем файл дабы прекратить повторные регистрации
 	}
 }
 
-$output = json_decode(file_get_contents('php://input'), true);
-file_put_contents('logs.txt', PHP_EOL . $output, FILE_APPEND);
-
-
 // обязательное. Запуск бота
 $bot->command('start', function ($message) use ($bot) {
-	//$datas = json_decode(file_get_contents("php://input"),true);
-	//file_put_contents('logs.txt', PHP_EOL . $datas, FILE_APPEND);
 	$answer = 'Добро пожаловать!';
-    $bot->sendMessage($message->getChat()->getId(), $answer);
+	$bot->sendMessage($message->getChat()->getId(), $answer);
 });
 
-// помощ
+// помощь
 $bot->command('help', function ($message) use ($bot) {
-    $answer = 'Команды:
-/help - помощ';
-    $bot->sendMessage($message->getChat()->getId(), $answer);
+	$answer = 'Команды:
+/help - помощь
+/random - сгенерировать случайное число от 0 до 100';
+	$bot->sendMessage($message->getChat()->getId(), $answer);
+});
+
+$bot->command('random', function ($message) use ($bot) {
+	$num = rand(0,100);
+	$answer = 'Случайное число: ' . $num;
+	$bot->sendMessage($message->getChat()->getId(), $answer);
 });
 
 // запускаем обработку
