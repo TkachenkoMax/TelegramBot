00df56f2addeab921cef6633eac5906b3701c67f
Max Tkachenko <manutd20x@gmail.com>
2017-08-12 10:43:50 +0300

    Migrations fix

--- Components/Migrations.php
+++ Components/Migrations.php
@@ -30,8 +30,8 @@ class Migrations{
                                             alias VARCHAR(100),
                                             city VARCHAR(100),
                                             PRIMARY KEY (id),
-                                            FOREIGN KEY (telegram_language) REFERENCES Languages(id) 
-                                            ON DELETE SET NULL
+                                            /*FOREIGN KEY (telegram_language) REFERENCES Languages(id) 
+                                            ON DELETE SET NULL */
                                         ) ENGINE=INNODB,
                                         CHARACTER SET utf8 COLLATE utf8_general_ci");
         
@@ -49,10 +49,10 @@ class Migrations{
                                             command_parameters JSON NULLABLE, 
                                             created_at TIMESTAMP,
                                             PRIMARY KEY (id),
-                                            FOREIGN KEY (id_user) REFERENCES users (id)
+                                           /* FOREIGN KEY (id_user) REFERENCES users (id)
                                             ON DELETE SET NULL,
                                             FOREIGN KEY (id_comand) REFERENCES commands (id)
-                                            ON DELETE SET NULL
+                                            ON DELETE SET NULL*/
                                         ) ENGINE=INNODB
                                         CHARACTER SET utf8 COLLATE uft8_general_ci");
     }
