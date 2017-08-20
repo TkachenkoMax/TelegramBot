a01c4ecf94dac5065c76a519f34f127b1f8a27e3
Max Tkachenko <manutd20x@gmail.com>
2017-08-20 17:00:15 +0300

    Kak zhe ya zaebalsya

--- Components/Application.php
+++ Components/Application.php
@@ -43,15 +43,15 @@ class Application {
     public function handle() 
     {
         $this->updates = $this->bot->run();
-        incoming_command($this->updates);
+        //incoming_command($this->updates);
 
         $this->user = UserModel::getBy("telegram_id", $this->updates[0]->getMessage()->getFrom()->getId())[0];
 
         if (!is_null($this->user) && $this->user->getIsAdmin()) {
             $controller = new AdminController();
 
-            $this->bot->command('migrate_up', $controller->migrateUp($this->bot));
-            $this->bot->command('migrate_down', $controller->migrateDown($this->bot));
+/*          $this->bot->command('migrate_up', $controller->migrateUp($this->bot));
+            $this->bot->command('migrate_down', $controller->migrateDown($this->bot));*/
             $this->bot->command('seed', $controller->seed($this->bot));
         }
         else 
@@ -66,7 +66,7 @@ class Application {
     }
     
     public function saveUpdateToDatabase(){
-        if (!is_null($this->user->getId())){
+        if (!is_null($this->user)){
             $data = array("id_user" => $this->user->getId(),
                 "message_id" => $this->updates[0]->getMessage()->getMessageId(),
                 "text_of_message" => $this->updates[0]->getMessage()->getText()
