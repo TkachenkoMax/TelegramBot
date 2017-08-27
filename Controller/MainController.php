545216060c01e089ec3d479af5e9e04e7ce11ed5
Max Tkachenko <manutd20x@gmail.com>
2017-08-27 17:44:04 +0300

    Inst

--- Controller/MainController.php
+++ Controller/MainController.php
@@ -413,32 +413,24 @@ class MainController
             $ig->setUser($instagram_account->getLogin(), $instagram_account->getPassword());
             $ig->login();
 
-            try {
-                // Get the UserPK ID for "natgeo" (National Geographic).
-                $userId = $ig->people->getUserIdForName('kurshakova_katerina');
-                // Starting at "null" means starting at the first page.
-                $maxId = null;
-                do {
-                    // Request the page corresponding to maxId.
-                    $response = $ig->timeline->getUserFeed($userId, $maxId);
-                    // In this example we're simply printing the IDs of this page's items.
-                    foreach ($response->getItems() as $item) {
-                        testFile("https://instagram.com/" . $item->getId() . $item->getCode());
-                    }
-                    // Now we must update the maxId variable to the "next page".
-                    // This will be a null value again when we've reached the last page!
-                    // And we will stop looping through pages as soon as maxId becomes null.
-                    $maxId = $response->getNextMaxId();
-                    // Sleep for 5 seconds before requesting the next page. This is just an
-                    // example of an okay sleep time. It is very important that your scripts
-                    // always pause between requests that may run very rapidly, otherwise
-                    // Instagram will throttle you temporarily for abusing their API!
-                    echo "Sleeping for 5s...\n";
-                    sleep(5);
-                } while ($maxId !== null); // Must use "!==" for comparison instead of "!=".
-            } catch (\Exception $e) {
-                echo 'Something went wrong: '.$e->getMessage()."\n";
-            }
+            $followers = [];
+
+            // Starting at "null" means starting at the first page.
+            $maxId = null;
+            do {
+                // Request the page corresponding to maxId.
+                $response = $ig->getSelfUserFollowers($maxId);
+
+                // In this example we're merging the response array, but we can do anything.
+                $followers = array_merge($followers, $response->getUsers());
+
+                // Now we must update the maxId variable to the "next page".
+                // This will be a null value again when we've reached the last page!
+                // And we will stop looping through pages as soon as maxId becomes null.
+                $maxId = $response->getNextMaxId();
+            } while ($maxId !== null); // Must use "!==" for comparison instead of "!=".
+
+            testFile($followers);
 
             $bot->sendMessage($user->getTelegramId(), "Успех!");
         };
