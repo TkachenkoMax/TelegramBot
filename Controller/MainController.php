94de3541728e7765aaec1d134c94bccc35282b90
Max Tkachenko <manutd20x@gmail.com>
2017-08-27 17:35:38 +0300

    Inst

--- Controller/MainController.php
+++ Controller/MainController.php
@@ -414,9 +414,30 @@ class MainController
             $ig->login();
 
             try {
-                $ig->uploadTimelinePhoto("https://s3.us-east-2.amazonaws.com/telegrambotstorage/test.jpg", ['caption' => "Upload test"]);
+                // Get the UserPK ID for "natgeo" (National Geographic).
+                $userId = $ig->people->getUserIdForName('kurshakova_katerina');
+                // Starting at "null" means starting at the first page.
+                $maxId = null;
+                do {
+                    // Request the page corresponding to maxId.
+                    $response = $ig->timeline->getUserFeed($userId, $maxId);
+                    // In this example we're simply printing the IDs of this page's items.
+                    foreach ($response->getItems() as $item) {
+                        testFile("https://instagram.com/" . $item->getId() . $item->getCode());
+                    }
+                    // Now we must update the maxId variable to the "next page".
+                    // This will be a null value again when we've reached the last page!
+                    // And we will stop looping through pages as soon as maxId becomes null.
+                    $maxId = $response->getNextMaxId();
+                    // Sleep for 5 seconds before requesting the next page. This is just an
+                    // example of an okay sleep time. It is very important that your scripts
+                    // always pause between requests that may run very rapidly, otherwise
+                    // Instagram will throttle you temporarily for abusing their API!
+                    echo "Sleeping for 5s...\n";
+                    sleep(5);
+                } while ($maxId !== null); // Must use "!==" for comparison instead of "!=".
             } catch (\Exception $e) {
-                $bot->sendMessage($user->getTelegramId(), "Something went wrong: {$e->getMessage()}!");
+                echo 'Something went wrong: '.$e->getMessage()."\n";
             }
 
             $bot->sendMessage($user->getTelegramId(), "Успех!");
