bce891c0f9173697990a91b88fd1ad1fe129def2
Max Tkachenko <manutd20x@gmail.com>
2017-08-28 12:46:27 +0300

    Heroku scheduler

--- public/test.php
+++ public/test.php
@@ -6,7 +6,19 @@
  * Time: 12:25
  */
 
-include_once "../Components/Scheduler.php";
+define('__ROOT__', require_once('../rootpath.php'));
+error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT);
+
+/**
+ * Launch autoloader
+ */
+$loader = require_once __ROOT__ . '/Components/Autoload.php';
+
+/**
+ * Launch Telegram Bot API
+ */
+
+require_once(__ROOT__ . "/vendor/autoload.php");
 
 $scheduler = new Scheduler();
 $scheduler->runTask();
