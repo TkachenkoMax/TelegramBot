4b3c230d34218844e2c9196720248cd8067068c1
Max Tkachenko <manutd20x@gmail.com>
2017-08-31 14:03:03 +0300

    Admin contorller

--- Controller/AdminController.php
+++ Controller/AdminController.php
@@ -107,9 +107,11 @@ class AdminController extends MainController
 
             file_put_contents($admins_file, "Список действующих администраторов:\n");
 
-            for($i = 0, $counter = 1; $i<count($admins); $i++, $counter++) {
-                if (!is_null($admins[$i]['deleted_at'])) {
-                    $current_admin_info = $counter . ") " . $admins[$i]['first_name'] . " " . $admins[$i]['last_name'] . " (Telegram ID - " . $admins[$i]['telegram_id'] . ")";
+            $counter = 1;
+
+            for($i = 0; $i<count($admins); $i++) {
+                if (is_null($admins[$i]['deleted_at'])) {
+                    $current_admin_info = $counter++ . ") " . $admins[$i]['first_name'] . " " . $admins[$i]['last_name'] . " (Telegram ID - " . $admins[$i]['telegram_id'] . ")";
 
                     file_put_contents($admins_file, $current_admin_info, FILE_APPEND);
 
